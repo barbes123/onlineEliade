3c103ad1e34315783fbc3ae525fb533814d68006
barbes123 <yes.coucou@gmail.com>
2022-05-13 11:19:28 +0300

    verified tools/TimeAlignmentTrigger.C

--- tools/TimeAlignmentTrigger.C
+++ tools/TimeAlignmentTrigger.C
@@ -21,41 +21,40 @@ void TimeAlignementTrigger(TH2 *matrix, float alignment_pos=0.)
   Int_t nb_bin_mat  = matrix->GetYaxis()->GetNbins();
   Float_t range_min = matrix->GetYaxis()->GetXmin();
   Float_t range_max = matrix->GetYaxis()->GetXmax();
- // std::cout<<"range_min "<<range_min<<" range_max "<<range_max<<"\n";
-  //TF1 *gaus = new TF1("gaus","gaus", -45e3, 45e3);//for mTimeCalib
- TF1 *gaus = new TF1("gaus","gaus", 0e3, 200e3);
-  //    gaus->SetParLimits(1,100e3,300e3);
-    //gaus->SetParLimits(0,30,100);
-    //gaus->SetParLimits(2,4500,7500);
-  TCanvas *c1 = new TCanvas("c1","c1");
-  TH1D *proj_y =new TH1D("projection","proj",nb_bin_mat,range_min,range_max);
 
-   for(int jj=0 ; jj<=matrix->GetXaxis()->GetNbins() ; jj++){
-    matrix->ProjectionY(proj_y->GetName(),jj,jj);
+ TF1 *gaus = new TF1("gaus","gaus", 0,100e3);
+ TF1 *gaus2 = new TF1("gaus","gaus", 0,100e3);
+ TCanvas *c1 = new TCanvas("c1","c1");
+ TH1D *proj_y =new TH1D("projection","proj",nb_bin_mat,range_min,range_max);
+ TH1D *proj_y2 =new TH1D("projection2","proj2",nb_bin_mat,range_min,range_max);
+ 
+ int delta = 50000;
+ 
+ for(int jj=0 ; jj<=matrix->GetXaxis()->GetNbins() ; jj++){
+ 
+    matrix->ProjectionY(proj_y->GetName(),jj,jj);    
     if (proj_y->GetEntries() == 0) continue;
-    proj_y->GetXaxis()->SetRangeUser(-100e3,600e3);
-    int max_bin = proj_y->GetMaximumBin();
-    float max_value = proj_y->GetXaxis()->GetBinCenter(max_bin);
-    c1->cd();
-//    gaus->SetParameter(1,alignment_pos);
-    //gaus->SetParLimits(1,4.0e+03,5e+03);
-//    proj_y->Fit(gaus,"MR");
-    proj_y->Draw();
-
-//    std::cout<<"max_value "<<max_value<<" max bin "<< proj_y-> GetBinCenter(max_bin-1) <<"\n";
-
-//    std::cout << "   Offset for domain " << j+channel_offset << "  " << gaus->GetParameter(1) << std::endl;;
-	//FIT
- //   std::cout << " domain " << jj-1<<" offset " << gaus->GetParameter(1) - alignment_pos << " maxbin_pos " << gaus->GetParameter(1) << std::endl;
-  //  outputFile << jj-1 << "  " << gaus->GetParameter(1) - alignment_pos <<"\n";;
-    //MAXBIN
-
-    //outputFile << j+channel_offset-1 << "  " << gaus->GetParameter(1) <<"\n";;
-//    outputFile << j+channel_offset-1 << "  " << max_value<< " offset: "<< alignment_pos - max_value <<"\n";;
-    outputFile << jj << "  " << proj_y-> GetBinCenter(max_bin) - alignment_pos <<"\n";;
-    std::cout << jj << "  " << proj_y-> GetBinCenter(max_bin) - alignment_pos <<"\n";;
+    proj_y->GetXaxis()->SetRangeUser(-50e3,150e3);
     
-   c1->WaitPrimitive();
+    proj_y->SetTitle(Form("dom%i",jj));
+    int max_bin = proj_y->GetMaximumBin();
+    int max_bin_pos = proj_y-> GetBinCenter(max_bin);  
+        
+    c1->cd();
+    gaus->SetParameter(1,max_bin_pos);
+    gaus->SetParLimits(1,max_bin_pos-2000,max_bin_pos+2000);
+    proj_y->Fit(gaus,"MR","",max_bin_pos-delta,max_bin_pos+delta);
+    
+    gaus2->SetParameter(1,gaus->GetParameter(1));
+    gaus2->SetParLimits(1,gaus->GetParameter(1) -1000,gaus->GetParameter(1) +1000);
+    
+    proj_y->Draw();
+   
+    std::cout<<" dom "<<jj-1<<" max_bin "<<max_bin<<" max bin "<< " max_bin_pos "<< max_bin_pos <<" gaus1 "<<gaus->GetParameter(1)<<" gaus2 "<< gaus2->GetParameter(1) <<"\n";
+    std::cout << " domain " << jj-1<<" offset " << proj_y-> GetBinCenter(max_bin) - alignment_pos << " maxbin_pos " << proj_y-> GetBinCenter(max_bin) << std::endl;;
+    outputFile << jj-1 << "  " << gaus->GetParameter(1) - alignment_pos <<"\n";;
+    
+    c1->WaitPrimitive();
     proj_y->Reset();
    }
     outputFile.close();
