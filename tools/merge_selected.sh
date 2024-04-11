93b71b20aa704d869fba06a374751c623a532f80
B420 <yes.coucou@gmail.com>
2024-04-11 14:43:13 +0300

    merge_selected.sh

---
+++ tools/merge_selected.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#File merging for the EliadeSelector files
+
+FIRSTrun=$1
+LASTrun=$2
+#volume1=$3
+#volume2=$4
+suffix=eliadeS2
+prefix=selected_run
+
+echo "Merge raw files for runs from $FIRSTrun to $LASTrun for all volumes "		
+
+space=" "
+
+runnb=$FIRSTrun
+
+while test $runnb -le $LASTrun
+do
+	echo "=== I am in run $runnb ==="		
+	volume1=0
+	volnb=$volume1
+	fileout="sum_$prefix""_$runnb""_$volume1""_$volume1""_$suffix"."root"
+	command="hadd sum_$prefix""_run$runnb""_$volume1""_$volume1""_$suffix"."root "
+
+
+	if test -f "$fileout"
+	   then
+	   rm "$fileout"
+	fi
+
+  	flag=1 #1 - file found
+	#current_file="run$runnb""_$volnb""_$suffix"".root"
+
+	#echo "First file in run $runnb is $current_file"
+
+
+	while test $flag -eq 1
+	 do 
+	 current_file="$prefix""_$runnb""_$volnb""_$suffix"".root"
+	 echo "current_file: $current_file"
+	   if test -f "$current_file"
+	   then
+	     command="$command""$current_file""$space"
+	     echo "$command"
+	     volnb=$(($volnb + 1))
+	     else 
+		flag=0
+	     break  
+	   fi  
+	done
+
+	echo "$command"
+	$command
+
+        volnb=$(($volnb - 1)) #the last version found
+#        filesave="sum_run$runnb""_$volume1""_$volnb""_$suffix"."root"
+        filesave="run$runnb""_999""_$suffix"."root"
+	mv $fileout $filesave
+     runnb=$(($runnb + 1))
+done
