3e6c54a2750c338e995ac6dbfec2eaf06e836718
vincent <vinc-19@hotmail.fr>
2024-05-21 17:38:41 +0300

    TA

--- tools/TimeAlignmentTrigger.C
+++ tools/TimeAlignmentTrigger.C
@@ -7,15 +7,17 @@
 #include "TF1.h"
 #include "TCanvas.h"
 #include "TString.h"
-void TimeAlignementTrigger(TH2 *matrix, float alignment_pos=0.)
+void TimeAlignementTrigger(TH2 *matrix, float alignment_pos=0., int start_domain = 0)
 {
 
   std::cout << "Welcome to the automatic TimeAlignement macro \n"
 	    << "I will calculate by default the offset to put in the LookUpTable\n"
 	    << "to put the coincidence peak in "<<alignment_pos <<" , except if you give a second parameter"
 	    << std::endl;
-  fstream outputFile;
-  outputFile.open("TimeCalib.dat", ios_base::out);		    
+  fstream outputFile1;
+ fstream outputFile2;
+  outputFile1.open("TimeCalibGaussian.dat", ios_base::out);		    
+  outputFile2.open("TimeCalibMaxBin.dat", ios_base::out);		    
 
   TString mat_name = matrix->GetName();
   Int_t nb_bin_mat  = matrix->GetYaxis()->GetNbins();
@@ -28,14 +30,15 @@ void TimeAlignementTrigger(TH2 *matrix, float alignment_pos=0.)
  TH1D *proj_y =new TH1D("projection","proj",nb_bin_mat,range_min,range_max);
  TH1D *proj_y2 =new TH1D("projection2","proj2",nb_bin_mat,range_min,range_max);
  
- int delta = 80000;
+ int delta = 10000;
  
  for(int jj=0 ; jj<=matrix->GetXaxis()->GetNbins() ; jj++){
  
+    if (jj < start_domain) continue;
     matrix->ProjectionY(proj_y->GetName(),jj,jj);    
     if (proj_y->GetEntries() == 0) continue;
 //    proj_y->GetXaxis()->SetRangeUser(-50e3,1e6);
-    proj_y->GetXaxis()->SetRangeUser(-1e6,1e6);
+    proj_y->GetXaxis()->SetRangeUser(-1e5,2e5);
     
     proj_y->SetTitle(Form("dom%i",jj));
     int max_bin = proj_y->GetMaximumBin();
@@ -43,7 +46,7 @@ void TimeAlignementTrigger(TH2 *matrix, float alignment_pos=0.)
         
     c1->cd();
     gaus->SetParameter(1,max_bin_pos);
-    gaus->SetParLimits(1,max_bin_pos-10000,max_bin_pos+10000);
+    gaus->SetParLimits(1,max_bin_pos-1000,max_bin_pos+1000);
     proj_y->Fit(gaus,"MR","",max_bin_pos-delta,max_bin_pos+delta);
     
     
@@ -54,10 +57,12 @@ void TimeAlignementTrigger(TH2 *matrix, float alignment_pos=0.)
    
     std::cout<<" dom "<<jj-1<<" max_bin "<<max_bin<<" max bin "<< " max_bin_pos "<< max_bin_pos <<" gaus1 "<<gaus->GetParameter(1)<<" gaus2 "<< gaus2->GetParameter(1) <<"\n";
     std::cout << " domain " << jj-1<<" offset " << proj_y-> GetBinCenter(max_bin) - alignment_pos << " maxbin_pos " << proj_y-> GetBinCenter(max_bin) << std::endl;;
-    outputFile << jj-1 << "  " << gaus->GetParameter(1) - alignment_pos <<"\n";;
+    outputFile1 << jj-1 << "  " << gaus->GetParameter(1) - alignment_pos <<"\n";;
+    outputFile2 << jj-1 << "  " <<  max_bin_pos - alignment_pos <<"\n";;
     
     c1->WaitPrimitive();
     proj_y->Reset();
    }
-    outputFile.close();
+    outputFile1.close();
+    outputFile2.close();
 }
