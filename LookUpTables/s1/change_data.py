75ec607b9c449507b4fa79e3577d03dc75027e45
B420 <yes.coucou@gmail.com>
2025-02-06 19:48:06 +0200

    lutz1

--- LookUpTables/s1/change_data.py
+++ LookUpTables/s1/change_data.py
@@ -1,16 +1,40 @@
 #! /usr/bin/python3
-import os, json
+import os, json, argparse
 
-with open('LUT_RECALL_S1_CL29_152Eu.json','r') as ifile:
-    js_data = json.load(ifile)
+def main():
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="Process a JSON file")
+    parser.add_argument('file', type=str, help="Name of the JSON file")
 
+    # Parse the command line arguments
+    args = parser.parse_args()
 
-for el in js_data:
-    el['fitLimits'] = [80,1500]
-    el['amp'] = 1000
-    el['PTLimits'] = [0, 1500]
+    # Get the filename from the argument
+    file_name = args.file
+    print(f"Processing file: {file_name}")
 
-js_new_data = json.dumps(js_data, indent=3)
+    try:
+        with open(file_name, 'r') as f:
+            js_data = json.load(f)
+            print("Data loaded successfully.")
+    except Exception as e:
+        print(f"Error reading the file: {e}")
 
-with open('LUT_RECALL_S1_CL29_152Eu.json','w') as ofile:
-    ofile.write(js_new_data)
+    for el in js_data:
+        if el['detType'] == 2:
+            el['fwhm'] = 2
+        # el['fitLimits'] = [80, 500]
+        #   el['amp'] = 1000
+        # el['PTLimits'] = [0, 1500]
+
+    js_new_data = json.dumps(js_data, indent=3)
+
+    try:
+        with open(f'{file_name}', 'w') as ofile:
+            ofile.write(js_new_data)
+            print("Data saved successfully.")
+    except Exception as e:
+        print(f"Error writing the file: {e}")
+
+if __name__ == "__main__":
+    main()
