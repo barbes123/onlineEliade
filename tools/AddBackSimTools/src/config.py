17b15d2e28e216a7d044a5851ea02a5a5b49e10e
B420 <yes.coucou@gmail.com>
2025-08-12 17:05:25 +0300

    all scipts for plots  are now in src

---
+++ tools/AddBackSimTools/src/config.py
@@ -0,0 +1,5 @@
+my_colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'orange', 'purple', 'brown']
+my_markers = ['o', 's', '^', 'v', 'x', '+', '*', 'D', 'p', ',']
+list_of_lut_2024 = {"S1":"CL33", "S2":"CL30", "S6":"CL31", "S3":"CL32", "S7":"CL29", "S5":"CL34", "S4":"CL35", "S8":"CL36"}
+list_of_runs = {'60Co': 208, '152Eu': 207,'133Ba':206}
+server = 6
